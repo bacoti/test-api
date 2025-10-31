58a3f29b687d9a15cf25b227025808d00d616972
Andi Aryanto <andi.aryanto007@gmail.com>
2025-10-30 22:41:34 -0700

    add new method parameters

--- script.js
+++ script.js
@@ -8,6 +8,28 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/parameters/:mhslpkia', (req, res) => {
+
+    let dataMhs=req.params.mhslpkia;
+    let listBlacklistMhs=["didin","panjul","pawaz"];
+
+    try {
+        if (listBlacklistMhs.find(x=>x===dataMhs).length>0 ){
+        return res.status(200).json({
+            "pesan":"anda masuk dalam blacklist",
+            "data_mhs_blacklist":listBlacklistMhs
+        })
+    }
+    } catch (error) {
+        
+    }
+    return res.status(200).json({
+            "pesan":"anda tidak termasuk dalam blacklist",
+            "data_mhs_blacklist":listBlacklistMhs
+    })
+
+})
+
 app.post('/json', (req, res) => {
   if (req.headers['content-type'] !== 'application/json') {
     return res.status(400).json({"errorMessage": "Invalid JSON"})
